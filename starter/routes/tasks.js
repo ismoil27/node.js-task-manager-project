2bd786e3bb551976ecdfc1391eb3a9385bfe3af4
MrIsmoiljon <abduraimovismoil98@gmail.com>
2022-06-09 17:58:45 +0900

    .

--- starter/routes/tasks.js
+++ starter/routes/tasks.js
@@ -2,7 +2,13 @@ const express = require("express");
 
 const router = express.Router();
 
-const { getAllTasks } = require("../controllers/tasks");
+const {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+} = require("../controllers/tasks");
 
 router.route("/").get(getAllTasks);
 
